de789366ae023cb9f33a08989db9b03a2879f806
Tony Spiro <tspiro@tonyspiro.com>
2018-04-18 12:48:39 -0500

    home fix

--- routes/home.js
+++ routes/home.js
@@ -4,14 +4,6 @@ module.exports = (app, config, partials) => {
   app.get('/', (req, res) => {
     Cosmic.getObjects({ bucket: { slug: config.COSMIC_BUCKET, read_key: config.COSMIC_READ_KEY } }, (err, response) => {
       res.locals.cosmic = response
-      const blurb_items = res.locals.cosmic.object.home.metafield.blurbs.children
-      blurb_items.forEach((item, i) => {
-        item.children.forEach((item_child, i) => {
-          if (item_child.type === 'file') {
-            item_child.imgix_url = 'https://cosmicjs.imgix.net/' + item_child.value
-          }
-        })
-      })
       res.locals.page = response.object.home
       return res.render('index.html', {
         partials
